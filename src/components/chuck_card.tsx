0b94698458367b0738011ec0008e306e96f85725
Merve <babutine@yahoo.com>
2023-12-05 18:04:13 +0000

    fix: deconstruct greeting in the props while passing to chuch_card

--- src/components/chuck_card.tsx
+++ src/components/chuck_card.tsx
@@ -9,14 +9,14 @@ interface ChuckCardProps {
 	jokes:Array<Joke>,
 }
 
-const ChuckCard: React.FC<ChuckCardProps> = (props) => {
+const ChuckCard: React.FC<ChuckCardProps> = ({chuckGreeting}) => {
 
 	return (
 		// 💡 This weird-looking `<>` thing is a "Fragment". Each component must have exactly one child.
 		// 💡 As a result, sometimes it is useful to wrap components in Fragments like this:
 		<>
 			{/* 💡 We can insert JavaScript into our JSX using curly braces like the ones below in the h2 */}
-			<h2>{ props.chuckGreeting }</h2>
+			<h2>{ chuckGreeting }</h2>
 
 			<img src={'Chuck image here!'} alt='A handsome man' />
 		</>
